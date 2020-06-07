2f7778f5ad75682c94d419b34f2971b398b4e38d
molincheng <1030658764@qq.com>
2020-06-07 15:16:05 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/css/global.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+// 配置请求的路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
