af8b3c0d1eacbee16a0620ac1eda46b1665849b3
molincheng <1030658764@qq.com>
2020-06-09 22:37:56 +0800

    '完成用户列表功能的开发'

--- src/main.js
+++ src/main.js
@@ -6,11 +6,20 @@ import router from './router'
 import './assets/css/global.css'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import './assets/font/iconfont.css'
 import axios from 'axios'
 
-Vue.prototype.$http = axios
 // 配置请求的路径
+// http://127.0.0.1:8888/api/private/v1/
+// https://www.liulongbin.top:8888/api/private/v1/
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+    console.log(config)
+        // 在最后必须return config
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    return config
+})
+Vue.prototype.$http = axios
 
 Vue.use(ElementUI);
 
@@ -18,8 +27,8 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
